0a2dd6cc49e3936dce237c13e999e2dd36e715b6
Trond Norbye <trond.norbye@gmail.com>
2020-03-26 09:41:50 +0100

    couch_compact: clang-tidy and clang-format fixup
    Preparation to add support for prototyping compaction for
    point in time recovery
    
    Change-Id: Iee65007edc018723942d06b01522fed9e09d44c7
    Reviewed-on: http://review.couchbase.org/124299
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- src/compactor.cc
+++ src/compactor.cc
@@ -1,18 +1,36 @@
+/*
+ *     Copyright 2020 Couchbase, Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+#include "bitfield.h"
 #include "couchstore_config.h"
 #include <libcouchstore/couch_db.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "bitfield.h"
 
-static void exit_error(couchstore_error_t errcode)
-{
+static void exit_error(couchstore_error_t errcode) {
     fprintf(stderr, "Couchstore error: %s\n", couchstore_strerror(errcode));
     exit(-1);
 }
 
 static void usage(const char* prog) {
-    fprintf(stderr, "Usage: %s [--purge-before <timestamp>] [--purge-only-upto-seq seq] [--dropdeletes] [--upgrade] <input file> <output file>\n", prog);
+    fprintf(stderr,
+            "Usage: %s [--purge-before <timestamp>] [--purge-only-upto-seq "
+            "seq] [--dropdeletes] [--upgrade] <input file> <output file>\n",
+            prog);
     exit(-1);
 }
 
@@ -23,7 +41,7 @@ typedef struct {
 } CouchbaseRevMeta;
 
 static int time_purge_hook(Db* target, DocInfo* info, sized_buf, void* ctx_p) {
-    time_purge_ctx* ctx = (time_purge_ctx*) ctx_p;
+    auto* ctx = static_cast<time_purge_ctx*>(ctx_p);
 
     if (info == nullptr) {
         /* Compaction finished */
@@ -31,12 +49,13 @@ static int time_purge_hook(Db* target, DocInfo* info, sized_buf, void* ctx_p) {
         return COUCHSTORE_SUCCESS;
     }
 
-    if(info->deleted && info->rev_meta.size >= 16) {
-        const CouchbaseRevMeta* meta = (const CouchbaseRevMeta*)info->rev_meta.buf;
+    if (info->deleted && info->rev_meta.size >= 16) {
+        const auto* meta =
+                reinterpret_cast<const CouchbaseRevMeta*>(info->rev_meta.buf);
         uint32_t exptime = decode_raw32(meta->expiry);
-        if(exptime < ctx->purge_before_ts
-           && (!ctx->purge_before_seq || info->db_seq <= ctx->purge_before_seq)) {
-            if(ctx->max_purged_seq < info->db_seq) {
+        if (exptime < ctx->purge_before_ts &&
+            (!ctx->purge_before_seq || info->db_seq <= ctx->purge_before_seq)) {
+            if (ctx->max_purged_seq < info->db_seq) {
                 ctx->max_purged_seq = info->db_seq;
             }
             return COUCHSTORE_COMPACT_DROP_ITEM;
@@ -46,70 +65,68 @@ static int time_purge_hook(Db* target, DocInfo* info, sized_buf, void* ctx_p) {
     return COUCHSTORE_COMPACT_KEEP_ITEM;
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     Db* source;
     couchstore_error_t errcode;
     time_purge_ctx timepurge = {0, 0};
-    couchstore_compact_hook hook = NULL;
-    couchstore_docinfo_hook dhook = NULL;
-    void* hook_ctx = NULL;
+    couchstore_compact_hook hook = nullptr;
+    couchstore_docinfo_hook dhook = nullptr;
+    void* hook_ctx = nullptr;
     int argp = 1;
     couchstore_compact_flags flags = 0;
     FileOpsInterface* target_io_ops = couchstore_get_default_file_ops();
-    if(argc < 3)
-    {
+    if (argc < 3) {
         usage(argv[0]);
     }
 
-    while(argv[argp][0] == '-') {
-        if(!strcmp(argv[argp],"--purge-before")) {
-            argp+=2;
-            if(argc + argp < 2) {
+    while (argv[argp][0] == '-') {
+        if (!strcmp(argv[argp], "--purge-before")) {
+            argp += 2;
+            if (argc + argp < 2) {
                 usage(argv[0]);
             }
             hook = time_purge_hook;
             hook_ctx = &timepurge;
-            timepurge.purge_before_ts = atoi(argv[argp-1]);
+            timepurge.purge_before_ts = atoi(argv[argp - 1]);
             printf("Purging items before timestamp %" PRIu64 "\n",
                    timepurge.purge_before_ts);
         }
 
-        if(!strcmp(argv[argp],"--purge-only-upto-seq")) {
-            if(argc - argp < 3) {
+        if (!strcmp(argv[argp], "--purge-only-upto-seq")) {
+            if (argc - argp < 3) {
                 usage(argv[0]);
             }
-            argp+=2;
-            timepurge.purge_before_seq = (uint64_t)(atoll(argv[argp-1]));
+            argp += 2;
+            timepurge.purge_before_seq = (uint64_t)(atoll(argv[argp - 1]));
             printf("Purging items only up-to seq %" PRIu64 "\n",
                    timepurge.purge_before_seq);
         }
 
-        if(!strcmp(argv[argp],"--dropdeletes")) {
+        if (!strcmp(argv[argp], "--dropdeletes")) {
             argp++;
-            if(argc + argp < 2) {
+            if (argc + argp < 2) {
                 usage(argv[0]);
             }
             flags |= COUCHSTORE_COMPACT_FLAG_DROP_DELETES;
         }
 
-        if(!strcmp(argv[argp], "--upgrade")) {
+        if (!strcmp(argv[argp], "--upgrade")) {
             argp++;
-            if(argc + argp < 2) {
+            if (argc + argp < 2) {
                 usage(argv[0]);
             }
             flags |= COUCHSTORE_COMPACT_FLAG_UPGRADE_DB;
         }
     }
 
-    errcode = couchstore_open_db(argv[argp++], COUCHSTORE_OPEN_FLAG_RDONLY, &source);
-    if(errcode)
-    {
+    errcode = couchstore_open_db(
+            argv[argp++], COUCHSTORE_OPEN_FLAG_RDONLY, &source);
+    if (errcode) {
         exit_error(errcode);
     }
-    errcode = couchstore_compact_db_ex(source, argv[argp], flags, hook, dhook, hook_ctx, target_io_ops);
-    if(errcode)
-    {
+    errcode = couchstore_compact_db_ex(
+            source, argv[argp], flags, hook, dhook, hook_ctx, target_io_ops);
+    if (errcode) {
         exit_error(errcode);
     }
 
@@ -117,8 +134,7 @@ int main(int argc, char** argv)
 
     errcode = couchstore_close_file(source);
     couchstore_free_db(source);
-    if(errcode)
-    {
+    if (errcode) {
         exit_error(errcode);
     }
 
